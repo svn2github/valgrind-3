f47354aba02ff4409ffcd0ef8121711d1d4cff27
sewardj <sewardj@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2006-09-19 13:18:28 +0000

    Delete this branch in preparation for resync with trunk.
    git-svn-id: svn://svn.valgrind.org/valgrind@6078 a5019735-40e9-0310-863c-91ae7b9d1cf9

--- branches/AIX5/memcheck/tests/execve2.c
+++
@@ -1,9 +0,0 @@
-#include <assert.h>
-#include <unistd.h>
-
-int main ( int argc, char** argv, char** envp )
-{
-   execve(NULL,        NULL, NULL);
-   execve("../../tests/true", NULL, envp);
-   assert(0);  // shouldn't get here
-}
